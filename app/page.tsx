fe51f55b0b6282105bcc8844df49164570d46276
Ryuk69 <ry609uk@gmail.com>
2024-03-09 02:50:03 +0530

    feat:  sigin/signup page, theme-change,navbar(incomplete)

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import { Button } from "@/components/ui/button"
-
+import Navbar from "@/components/Navbar"
 export default function Home() {
   return (
     <div>
+      <Navbar />
+      {/* <h1>hii2</h1> */}
       <Button>Click me</Button>
     </div>
   )
