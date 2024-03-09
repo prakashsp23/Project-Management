6ec9b3b1f9d6ad999282c635617a3bc51cd90b4d
Ryuk69 <ry609uk@gmail.com>
2024-03-09 17:37:19 +0530

    fix: navbar(90%)

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,141 +1,81 @@
-
-// import Upload from './Upload';
-
+"use client"
 import { Button } from "./ui/button";
 import { ModeToggle } from "./ui/toggle-mode";
-
-// function Navbar() {
-//   return (
-//     <div classNameName="bg-[#121212] flex flex-col items-center p-40 py-4  md:p-1 ">
-//       <div classNameName="navbar container mx-auto max-w-7xl flex justify-between items-center">
-//         <a
-//           href="/"
-//           classNameName="font-aquatico tracking-wider text-3xl font-bold text-white"
-//         >
-//           {/* <span classNameName="font-anurati text-5xl">P</span> */}
-//           Project
-//         </a>
-//         <ul classNameName="flex space-x-4">
-//           <li>
-//             <a href="" classNameName="text-white">
-//               Team
-//             </a>
-//           </li>
-//           <li>
-//             <a href="" classNameName="text-white">
-//             Notification
-//             </a>
-//           </li>
-//           <li>
-//             <a href="" classNameName="text-white md:pr-3">
-//             Projects
-//             </a>
-//           </li>
-//         </ul>
-//       </div>
-//       {/* Upload Component */}
-//       {/* <Upload /> */}
-//       {/* <About /> */}
-//     </div>
-//   );
-// }
-
-// export default Navbar;
-// "use client"
-
-// import * as React from "react"
-// import Link from "next/link"
-// import { usePathname } from "next/navigation"
-
-// // import { siteConfig } from "@/config/site"
-// import { cn } from "@/lib/utils"
-// // import { Icons } from "@/components/icons"
-// // import { Badge } from "@/registry/new-york/ui/badge"
-
-// export default function Navbar() {
-//   const pathname = usePathname()
-
-//   return (
-//     <div classNameName="p-3 hidden md:flex border-2	border-black	">
-//       {/* <Link href="/" classNameName="mr-6 flex items-center space-x-2">
-//         <Icons.logo classNameName="h-6 w-6" />
-//         <span classNameName="hidden font-bold sm:inline-block">
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+import { cn } from "@/lib/utils"
+function Navbar() {
+  const pathname = usePathname()
+  return (
+    <div className="p-3 hidden md:flex border-2 	border-black	">
+{/* //       <Link href="/" className="mr-6 flex items-center space-x-2">
+//         <Icons.logo className="h-6 w-6" />
+//         <span className="hidden font-bold sm:inline-block">
 //           {siteConfig.name}
 //         </span>
 //       </Link> */}
-//       <nav classNameName="flex items-center gap-6 text-sm">
-//         <Link
-//           href="/team"
-//           classNameName={cn(
-//             "transition-colors hover:text-foreground/80",
-//             pathname === "/team" ? "text-foreground" : "text-foreground/60"
-//           )}
-//         >
-//           Team
-//         </Link>
-//         <Link
-//           href="/projects"
-//           classNameName={cn(
-//             "transition-colors hover:text-foreground/80",
-//             pathname?.startsWith("/projects")
-//               ? "text-foreground"
-//               : "text-foreground/60"
-//           )}
-//         >
-//           Projects
-//         </Link>
-//         <Link
-//           href="/themes"
-//           classNameName={cn(
-//             "transition-colors hover:text-foreground/80",
-//             pathname?.startsWith("/themes")
-//               ? "text-foreground"
-//               : "text-foreground/60"
-//           )}
-//         >
-//           Notification
-//         </Link>
-//         <Link
-//           href="/examples"
-//           classNameName={cn(
-//             "transition-colors hover:text-foreground/80",
-//             pathname?.startsWith("/examples")
-//               ? "text-foreground"
-//               : "text-foreground/60"
-//           )}
-//         >
-//           Chat
-//         </Link>
-//         {/* <Link
-//         //   href={siteConfig.links.github}
-//           classNameName={cn(
-//             "hidden text-foreground/60 transition-colors hover:text-foreground/80 lg:block"
-//           )}
-//         >
-//           GitHub
-//         </Link> */}
-//       </nav>
-//     </div>
-//   )
-// }
-export default function Nav(){
-  return (
-    // <div>hii</div>
-    <header>
-      <nav>
-        <ul className="flex items-center justify-between p-4">
-          <li className="">
-            <a className="m-2">Team</a>
-            <a className="m-2">Projects</a>
-            <a className="m-2">Notification</a>
-            </li>
-          <li><ModeToggle/></li>
-        </ul>
-        
+      <nav className=" items-center grid grid-cols-8 gap-6 text-sm">
+        <div className="col-start-2 col-span-1">Project Name</div>
+        <div className="flex justify-end col-start-8 items-center col-span-2">
+          <Link
+          href="/team"
+          className={cn(
+            "transition-colors hover:text-foreground/100 px-4",
+            pathname === "/team" ? "text-foreground" : "text-foreground/60"
+          )}
+        >
+          Team
+        </Link>
+        <Link
+          href="/projects"
+          className={cn(
+            "transition-colors hover:text-foreground/100 px-4",
+            pathname?.startsWith("/projects")
+              ? "text-foreground"
+              : "text-foreground/60"
+          )}
+        >
+          Projects
+        </Link>
+        <Link
+          href="/notification"
+          className={cn(
+            "transition-colors hover:text-foreground/100 px-4",
+            pathname?.startsWith("/Notification")
+              ? "text-foreground"
+              : "text-foreground/60"
+          )}
+        >
+          Notification
+        </Link>
+        <Link
+          href="/chat"
+          className={cn(
+            "transition-colors hover:text-foreground/100 px-4",
+            pathname?.startsWith("/Chat")
+              ? "text-foreground"
+              : "text-foreground/60"
+          )}
+        >
+          Chat
+        </Link>
+        {/* <Link
+          href="https://www.w3schools.com/"
+          className={cn(
+            "hidden text-foreground/60 transition-colors hover:text-foreground/80 lg:block"
+          )}
+        >
+          GitHub
+        </Link> */}
+        <div className="px-4"><ModeToggle/></div>
+        </div>
       </nav>
-    </header>
-  )
+      
+    </div>
+  );
 }
+
+export default Navbar;
 
 
 
